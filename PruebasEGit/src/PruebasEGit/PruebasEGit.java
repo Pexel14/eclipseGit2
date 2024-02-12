257d70c5c382732712f218b273a9a928b8532264
Axel Leon <axeleon76@gmail.com>
2024-02-12 11:21:07 +0100

    Primer Commit rama1

--- PruebasEGit/src/PruebasEGit/PruebasEGit.java
+++ PruebasEGit/src/PruebasEGit/PruebasEGit.java
@@ -5,6 +5,7 @@ public class PruebasEGit {
 	public static void main(String[] args) {
 		System.out.println("Primer commit");
 		System.out.println("Segundo commit master");
+		System.out.println("Primer commit rama1");
 	}
 
 }
