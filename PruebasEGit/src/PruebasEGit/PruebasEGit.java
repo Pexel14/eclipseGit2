ecc848cf0d9de8fea5c17270f4c53e6fed2f495a
Axel Leon <axeleon76@gmail.com>
2024-02-12 11:22:44 +0100

    Segundo commit rama1

--- PruebasEGit/src/PruebasEGit/PruebasEGit.java
+++ PruebasEGit/src/PruebasEGit/PruebasEGit.java
@@ -6,6 +6,7 @@ public class PruebasEGit {
 		System.out.println("Primer commit");
 		System.out.println("Segundo commit master");
 		System.out.println("Primer commit rama1");
+		System.out.println("Segundo commit rama1");
 	}
 
 }
